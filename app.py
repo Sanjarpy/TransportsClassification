b1f273680451bb2820a03c4a7d8e6465762b7c9e
Sanjarpy <140885981+Sanjarpy@users.noreply.github.com>
2023-09-28 16:51:08 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+temp=pathlib.PosixPath
+pathlib.PosixPath=pathlib.WindowsPath
+st.title('Transportni klassifikatsiya qiluvchi model')
+file=st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
+if file:
+    img= PILImage.create(file)
+    model=load_learner('transport_model.pkl')
+
+    pred, pred_id, probs=model.predict(img)
+
+    st.success(f"bashorat: {pred}")
+    st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}%')
+
+    st.image(file)
+    #plotting  
+    fig=px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)  
